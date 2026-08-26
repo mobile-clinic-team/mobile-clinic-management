37d75a08c2c48856b31630a27535d12b4dea82a5
Đoàn Quốc Việt <doanquocviet30062006@gmail.com>
2026-08-26 17:09:41 +0700

    fix(android): remove BOM from libs.versions.toml and gradlew.bat (Gradle TOML parse error)

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -1,16 +1,22 @@
-﻿@rem ##########################################################################
+@rem ##########################################################################
 @rem  Gradle startup script for Windows
 @rem ##########################################################################
 @if "%DEBUG%"=="" @echo off
-setlocal
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
 set DIRNAME=%~dp0
+if "%DIRNAME%"=="" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
 if "%ERRORLEVEL%"=="0" goto init
-echo ERROR: JAVA_HOME is not set correctly.
+
+echo.
+echo ERROR: JAVA_HOME is not set and no '"java"' command could be found in your PATH.
+echo.
 goto fail
 
 :init
@@ -19,8 +25,14 @@ set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 :execute
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+
+:end
 if "%ERRORLEVEL%"=="0" goto mainEnd
+
 :fail
 exit /b 1
+
 :mainEnd
 if "%OS%"=="Windows_NT" endlocal
+
+:omega
