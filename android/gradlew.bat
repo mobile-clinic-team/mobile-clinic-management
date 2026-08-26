897a99e47d4057caf6c1189e980365305c5a3385
Đoàn Quốc Việt <doanquocviet30062006@gmail.com>
2026-08-26 17:05:56 +0700

    build(android): add gradle wrapper (gradlew.bat, gradle-wrapper.jar, properties)

---
+++ android/gradlew.bat
@@ -0,0 +1,26 @@
+﻿@rem ##########################################################################
+@rem  Gradle startup script for Windows
+@rem ##########################################################################
+@if "%DEBUG%"=="" @echo off
+setlocal
+set DIRNAME=%~dp0
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+if "%ERRORLEVEL%"=="0" goto init
+echo ERROR: JAVA_HOME is not set correctly.
+goto fail
+
+:init
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+:execute
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+if "%ERRORLEVEL%"=="0" goto mainEnd
+:fail
+exit /b 1
+:mainEnd
+if "%OS%"=="Windows_NT" endlocal
